48e106745c2969a68f984ef4c4ae8c4e67763f55
Dustin Stringer <dustin.stringer@spaceforce.mil>
2023-04-27 10:41:36 -0400

    Added stuff and things

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -1,48 +1,62 @@
 <script setup>
-import './assets/main.css'
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import "./assets/main.css";
+import WeatherData from "./components/WeatherData.vue";
+
+const props = defineProps({
+  partId: String,
+});
+
+console.log(props.partId);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <WeatherData :partId="props.partId" />
   </main>
 </template>
 
+
 <style>
-header {
-  line-height: 1.5;
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  /* background-image: url('https://i0.wp.com/stationproject.blog/wp-content/uploads/2012/08/weather-station-18.jpg?ssl=1'); */
+  /* background-size: cover; */
+  background-color: black;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper h1,
+h2,
+h5 {
+  color: var(--pal-v1-status-e-200);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+table {
+  width: 100%;
+  border: 1px solid var(--pal-v1-neutrals-300);
+  font-size: 0.75rem;
+  border-collapse: collapse;
+  color: white;
+}
+table thead {
+  background-color: var(--pal-v1-status-e-200);
+}
+table tr {
+  border-top: 1px solid var(--pal-v1-text-600);
+  transition: all ease-in-out 100ms;
+}
+table th {
+  text-align: left;
+  padding: 0.5rem;
+}
+table td {
+  padding: 0.5rem;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+table tr:hover {
+  background-color: var(--pal-v1-accent-500);
 }
 </style>
---
+++ src/components/WeatherData.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref } from "vue";
+import weatherDataJSON from "./weather.json";
+
+const weatherAPI = "http://localhost:3000/weather";
+
+const towersIds = {
+  Tower_0003_eiffel: "0003",
+  "Tower_0006_eiffel.002": "0006NW",
+  "Tower_0303_eiffel.001": "0303",
+};
+
+const props = defineProps({
+  partId: String,
+});
+
+const towerId = towersIds[props.partId];
+const weatherData = ref([]);
+
+// Collect every weatherDataJSON with the secified tower and sort by HGT
+weatherData.value = weatherDataJSON
+  .filter((data) => data.TOWER === towerId)
+  .sort((a, b) => b.HGT - a.HGT)
+  // Remove LAT, LON, AVG
+  .map((data) => {
+    const { LAT, LON, AVG, TOWER, AV, ...rest } = data;
+    return rest;
+  })
+  // Remove any field that contains no value across all data
+  .map((data) => {
+    const newData = {};
+    Object.keys(data).forEach((key) => {
+      if (data[key] !== "") {
+        newData[key] = data[key];
+      }
+    });
+    return newData;
+  });
+</script>
+
+<template>
+  <div class="wrapper">
+    <h2>WINDS Data</h2>
+    <h5>Tower: {{ towerId }}</h5>
+    <!-- A table of wind tower data with the HGT value on the left and the rest of the data following, with headers on top -->
+    <table>
+      <thead>
+        <tr>
+          <th v-for="key in Object.keys(weatherData[0])" :key="key">
+            {{ key }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- // Iterate through each value of the weatherData array -->
+        <tr v-for="data in weatherData" :key="data.HGT">
+          <!-- // Iterate through each value of the data object -->
+          <td v-for="value in Object.values(data)" :key="value">{{ value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
